557386f41eb4fd485664320025d8221408102666
AzureDVBB <24957704+AzureDVBB@users.noreply.github.com>
2020-07-27 17:47:13 +0200

    Experimented with runtime module reloading in 'src/mud/_study/Runtime Module Reloading' successfully. See 'explanation.txt.' for more information and insights.

---
+++ src/mud/_study/Runtime Module Reloading/reloader.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 15:38:36 2020
+
+@author: AzureD
+
+Testing reloading of modules during runtime to commit changes made without restarting scripts.
+"""
+
+import testing # load test module
+import packtest # load test package
+
+import time
+import importlib
+
+variable_example = testing.run # test if taken references will change
+
+def reload_all():
+    importlib.reload(testing)
+    importlib.reload(packtest)
+
+while True:
+    variable_example()
+    testing.run()
+    packtest.testing.run()
+    time.sleep(10)
+    reload_all()
